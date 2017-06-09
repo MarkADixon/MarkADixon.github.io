347f0695115c9bef9793596921e8f1f29e2b82fc
Colt M. Bauer <bauercolt@gmail.com>
2017-06-08 23:52:52 -0400

    new UI

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@
 
 $(document).ready( function (){
   var homeSymbol = $(".home__symbol");  
+  var homeContainer = $(".home__container");
 
-
-
-
+  
+  homeSymbol.click( function (event){ 
+    homeContainer.toggleClass("color-primary--invert");
+  });
 
 
 
