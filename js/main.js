0a0fda0aada8d5f09acc755f4132bbdc98ee5362
Colt M. Bauer <bauercolt@gmail.com>
2017-06-13 23:34:58 -0400

    about page structure

--- js/main.js
+++ js/main.js
@@ -1,55 +1,58 @@
-var viewSection = "home";
+var viewSection = "#home";
 
 
 
 
 $(document).ready( function (){
   var homeSymbol = $(".home__symbol");  
-  var homeContainer = $(".home__container");
-  var mainArea = $("main");
-  var navButtons = $(".nav-main__button, .nav-mobile__button");
+  var homeContainer = $(".home__container")
+  var navButton = $(".nav__button");
+  var cards = $(".card")
   
-  homeSymbol.click( function (event){ 
-    homeContainer.toggleClass("color-page--invert");
+  
+  homeSymbol.on('click', function(event) { 
+    homeContainer.toggleClass("color-page color-page--invert");
   });
-  homeContainer.click( function (event){ 
-    $(this).toggleClass("spin");
+
+  cards.on('click', function(event) { 
+    cards.toggleClass("color-page color-page--invert");
   });
-  navButtons.click( function (event){
-    //if already selected button was clicked, return doing nothing
-    if($(this).hasClass("nav-main__button--selected") || $(this).hasClass("nav-mobile__button--selected")) { return; }
-    //spin animation 
-    var icon = $(this).children(".nav-main__icon, .nav-mobile__icon")
-    icon.toggleClass("spin");
-    //deselect class changes
+
+  //view changes on navigation click
+  navButton.on('click', function(event) { 
+    var selectSection = "#"+this.dataset.linkto;
+    var selectSectionButtons = ".button__"+this.dataset.linkto;
+    //cancel if destination view is same as current view
+    if (viewSection === selectSection) { return; }
+    //update to the destination view
+    viewSection = selectSection;
+    $("section").removeClass("section--show");
+    $(selectSection).addClass("section--show");
+    //nav menu deselect class changes
     $(".nav-main__button--selected, .nav-mobile__button--selected").removeClass("nav-main__button--selected nav-mobile__button--selected");
     $(".nav-main__icon-top, .nav-mobile__icon-top").removeClass("color-button--invert nav-main__icon-top--selected nav-mobile__icon-top--selected");
     $(".nav-main__icon-base, .nav-mobile__icon-base").removeClass("color-button").addClass("color-button--invert");
     $(".button__hire .nav-main__icon-base").addClass("color-cta");
     $(".button__hire .nav-mobile__icon-base").addClass("color-cta");
-    //flag section change
-    if ($(this).hasClass("button__home")) viewSection = "home";
-    else if ($(this).hasClass("button__about")) viewSection = "about";
-    else if ($(this).hasClass("button__resume")) viewSection = "resume";
-    else if ($(this).hasClass("button__portfolio")) viewSection = "portfolio";
-    else if ($(this).hasClass("button__hire")) viewSection = "hire";
-    //make transistion class changes on nav menus
-    var selectString = ".button__"+viewSection;
-    $(selectString).each( function () {
+    //nav menu selected class changes 
+    $(selectSectionButtons).each( function () {
       //selected transition
       if ($(this).hasClass("nav-main__button")) {
         $(this).addClass("nav-main__button--selected");
         $(this).find(".nav-main__icon-top").addClass("color-button--invert nav-main__icon-top--selected");
         $(this).find(".nav-main__icon-base").removeClass("color-button--invert color-cta").addClass("color-button");
+        //spin animation 
+        $(this).find(".nav-main__icon").toggleClass("spin");
       }
       if ($(this).hasClass("nav-mobile__button")) {
         $(this).addClass("nav-mobile__button--selected");
         $(this).find(".nav-mobile__icon-top").addClass("color-button--invert nav-mobile__icon-top--selected");
         $(this).find(".nav-mobile__icon-base").removeClass("color-button--invert color-cta").addClass("color-button");
+        //spin animation 
+        $(this).find(".nav-mobile__icon").toggleClass("spin");
       }
     });
   });
-
 
 
 
